a4ee5115e83a61504dcbd0d15747f25f04ad194d
JohnFury77 <furijani99@gmail.com>
2020-09-29 12:56:38 +0200

    megküzd, override

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -25,7 +25,7 @@ namespace HarcosProjekt
                 case 1: this.alapEletero = 15; this.alapSebzes = 3; break;
                 case 2: this.alapEletero = 12; this.alapSebzes = 4; break;
                 case 3: this.alapEletero = 8; this.alapSebzes = 5; break;
-                default: this.alapEletero = 15; this.alapSebzes = 3; break;
+                default: this.alapEletero = 12; this.alapSebzes = 4; break;
             }
         }
 
@@ -36,6 +36,45 @@ namespace HarcosProjekt
         public int AlapEletero { get => alapEletero; }
         public int AlapSebzes { get => alapSebzes; }
         public int Sebzes { get => alapSebzes+szint; }
+        public int SzintLepeshez { get => 10 + szint * 5; }
+        public int MaxEletero { get => alapEletero + szint * 3; }
+
+        public override string ToString()
+        {
+            return string.Format(nev + ":\nLVL: " + szint + "\nEXP: " + tapasztalat + "/" + SzintLepeshez + "\nHP: " + eletero + "/" + MaxEletero + "\nDMG: " + Sebzes);
+        }
+        public void Megkuzd(Harcos masikHarcos, Harcos harcos)
+        {
+            if (masikHarcos==harcos)
+            {
+                Console.WriteLine("Ez nem az a játék.");
+            }
+            else if (masikHarcos.eletero==0||harcos.eletero==0)
+            {
+                Console.WriteLine("Ő már meghalt.");
+            }
+            masikHarcos.eletero -= harcos.Sebzes;
+            if (masikHarcos.eletero>0)
+            {
+                harcos.eletero -= masikHarcos.Sebzes;
+            }
+            if (harcos.eletero > 0) 
+            {
+                harcos.tapasztalat += 5;
+            }
+            if (masikHarcos.eletero > 0) 
+            {
+                masikHarcos.tapasztalat += 5;
+            }
+            if (harcos.eletero==0)
+            {
+                masikHarcos.tapasztalat += 10;
+            }
+            if (masikHarcos.eletero==0)
+            {
+                harcos.tapasztalat += 10;
+            }
+        }
 
 
     }
